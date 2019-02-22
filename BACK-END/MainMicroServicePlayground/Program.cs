b9edf93e54b81831c53bf0face8ab32d62b019ce
Linh Nguyen <redplane_dt@yahoo.com.vn>
2019-02-22 23:47:20 +0800

    - Added a project that is used for testing external api call.

---
+++ BACK-END/MainMicroServicePlayground/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MainMicroServicePlayground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
